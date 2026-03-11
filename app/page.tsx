510ec7898111cd816f8725559edf4212858efa93
Shiv03072002 <ushiv.kumar@7lives.agency>
2026-03-11 15:30:20 +0530

    resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import HeroSection from '@/components/home/HeroSection'
 import Section from '@/components/home/Section'
-import ScatteredImages from '@/components/home/ScatteredImages'
+
 export default function Home() {
   return (
     <div className="">
